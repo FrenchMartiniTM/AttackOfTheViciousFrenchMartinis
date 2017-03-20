fed0012e341c78285edb7b07ff4f85bbfd32e883
Dobroto <georg.borislav@gmail.com>
2017-03-20 15:03:05 +0200

    1. Added comments for unclear commands. 2. Changed the place of explosions.

--- game.js
+++ game.js
@@ -18,7 +18,7 @@ var bullets;
 var fireButton;
 var bulletTimer = 0;
 var greenEnemies;
-var FACTOR_DIFFICULTY = 1;//TODO: Set with score or etc.
+var FACTOR_DIFFICULTY = 1; //TODO: Set with score or etc.
 var explosions;
 var EXPLOSION_SPEED = 5;
 
@@ -28,7 +28,7 @@ function preload() {
     game.load.image('player', 'assets/images/Bartender_80_88_invert.png');
     game.load.image('bullet', 'assets/images/green_olive_15_19.png');
     game.load.image('enemy-green', 'assets/images/glass_80_115_rotated.png');
-    game.load.spritesheet('explosion', 'assets/images/explode1.png', 128, 128);
+    game.load.spritesheet('explosion', 'assets/images/explode.png', 128, 128);
 }
 
 function create() {
@@ -73,7 +73,7 @@ function create() {
     explosions.createMultiple(30, 'explosion');
     explosions.setAll('anchor.x', 0.5);
     explosions.setAll('anchor.y', 0.5);
-    explosions.forEach(function (explosion) {
+    explosions.forEach(function(explosion) {
         explosion.animations.add('explosion');
     });
     //  The baddies!
@@ -81,16 +81,20 @@ function create() {
     greenEnemies.enableBody = true;
     greenEnemies.physicsBodyType = Phaser.Physics.ARCADE;
     greenEnemies.createMultiple(5 * FACTOR_DIFFICULTY, 'enemy-green'); //TODO: can add factor to number of enemies in dependence to difficutly level
-    greenEnemies.setAll('anchor.x', 0.5);//TODO: What is this for
-    greenEnemies.setAll('anchor.y', 0.5);//TODO: What is this for
+    greenEnemies.setAll('anchor.x', 0.5); //places the anchor in the exact middle of the sprite, horizontally and vertically.
+    greenEnemies.setAll('anchor.y', 0.5); //places the anchor in the exact middle of the sprite, horizontally and vertically.
     greenEnemies.setAll('scale.x', 0.5);
     greenEnemies.setAll('scale.y', 0.5);
     greenEnemies.setAll('angle', 180);
-    greenEnemies.setAll('outOfBoundsKill', true);
-    greenEnemies.setAll('checkWorldBounds', true);
+    greenEnemies.setAll('outOfBoundsKill', true); //the object is killed when out of the boundaries
+    greenEnemies.setAll('checkWorldBounds', true); // it checks every time if it's out of the bounds
 
+    greenEnemies.forEach(function(enemy) {
+        enemy.body.setSize(enemy.width * 3 / 4, enemy.height * 3 / 4);
+    });
 
     launchGreenEnemy();
+
     function launchGreenEnemy() {
         //*/moved to main variables for modification of difficulty by score or other
         var MIN_ENEMY_SPACING = 1000 / FACTOR_DIFFICULTY; //TODO: can work with difficulty
@@ -98,7 +102,8 @@ function create() {
         var ENEMY_SPEED = 100 * FACTOR_DIFFICULTY; //TODO: can work with difficulty
         var enemy = greenEnemies.getFirstExists(false);
         if (enemy) {
-            enemy.reset(game.rnd.integerInRange(+100, game.width - 100), 0); //TODO: What is this for
+            enemy.reset(game.rnd.integerInRange(+100, game.width - 100), 0); //The Reset component allows a Game Object to be reset 
+            //and repositioned to a new location.
             enemy.body.velocity.x = game.rnd.integerInRange(0, 0);
             enemy.body.velocity.y = ENEMY_SPEED;
             enemy.body.drag.x = 100;
@@ -111,6 +116,7 @@ function create() {
 
     }
 }
+
 function update() {
     //  Scroll the background
     //bar.tilePosition.y = 2; //ours is stationary
@@ -191,18 +197,17 @@ function update() {
     if (cursors.left.isDown) {
         player.body.velocity.x = -MAXSPEED; //without smootness of movement
         //player.body.acceleration.x = -ACCLERATION; //with up
-    }
-    else if (cursors.right.isDown) {
-        player.body.velocity.x = MAXSPEED;//without smootness of movement
+    } else if (cursors.right.isDown) {
+        player.body.velocity.x = MAXSPEED; //without smootness of movement
         //player.body.acceleration.x = ACCLERATION;
     }
-//  Fire bullet
+    //  Fire bullet
     if (fireButton.isDown || game.input.activePointer.isDown) {
         fireBullet();
     }
 
 
-//  Stop at screen edges
+    //  Stop at screen edges
     if (player.x > game.width - 50) {
         player.x = game.width - 50;
         //player.body.acceleration.x = 0;//with smootness of movement
@@ -218,7 +223,8 @@ function update() {
 function render() {
 
 }
-function playerCollide(player, enemy) {//TODO: Player live is set to false => GAME OVER
+
+function playerCollide(player, enemy) { //TODO: Player live is set to false => GAME OVER
     var explosion = explosions.getFirstExists(false);
     explosion.reset(enemy.body.x + enemy.body.halfWidth, enemy.body.y + enemy.body.halfHeight);
     explosion.body.velocity.y = enemy.body.velocity.y;
@@ -226,6 +232,7 @@ function playerCollide(player, enemy) {//TODO: Player live is set to false => GA
     explosion.play('explosion', EXPLOSION_SPEED, false, true);
     enemy.kill();
 }
+
 function hitEnemy(enemy, bullet) {
     var explosion = explosions.getFirstExists(false);
     explosion.reset(bullet.body.x + bullet.body.halfWidth, bullet.body.y + bullet.body.halfHeight);
