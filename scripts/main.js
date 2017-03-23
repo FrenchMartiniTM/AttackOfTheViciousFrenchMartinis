3093f7fa6b8f2aa17f71c8ae8af421e499133953
gchankov <gchankov@gmail.com>
2017-03-23 08:04:11 +0200

    Cleanup

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,1 @@
-var AttackOfTheViciousFrenchMartinis = AttackOfTheViciousFrenchMartinis || {};
-
-AttackOfTheViciousFrenchMartinis.game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-demo');
-
-AttackOfTheViciousFrenchMartinis.game.state.add('Boot', AttackOfTheViciousFrenchMartinis.Boot);
-AttackOfTheViciousFrenchMartinis.game.state.add('Preload', AttackOfTheViciousFrenchMartinis.Preload);
-AttackOfTheViciousFrenchMartinis.game.state.add('MainMenu', AttackOfTheViciousFrenchMartinis.MainMenu);
-AttackOfTheViciousFrenchMartinis.game.state.add('Game', new GameState(AttackOfTheViciousFrenchMartinis.game));
-
-AttackOfTheViciousFrenchMartinis.game.state.start('Boot');
+//Insert game manager here...
