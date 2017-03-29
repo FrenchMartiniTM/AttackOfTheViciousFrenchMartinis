e5b25ee722c9340a8ee0e8b6b58c3191c74fa2e9
RosenUrkov <rosen.urkov@gmail.com>
2017-03-29 17:48:08 +0300

    Refactoring weapon

--- scripts/game-models/bartender.js
+++ scripts/game-models/bartender.js
@@ -1,29 +1,28 @@
 class Bartender extends Phaser.Sprite {
-  constructor(game, image) {
-    super(game, PLAYER.STARTING_POSITION_X, PLAYER.STARTING_POSITION_Y, image);
-    this.anchor.setTo(0.5, 0.5);
-    this.lives = 3;
-    game.physics.enable(this, Phaser.Physics.ARCADE);
-    this.body.maxVelocity.setTo(PLAYER.MAX_SPEED, PLAYER.MAX_SPEED);
-    game.add.existing(this);
-  }
-
-  update() {
-    //  Stop at screen edges
-    if (this.x > this.game.width - 50) {
-      this.x = this.game.width - 50;
+    constructor(game, image) {
+        super(game, PLAYER.STARTING_POSITION_X, PLAYER.STARTING_POSITION_Y, image);
+        this.anchor.setTo(0.5, 0.5);
+        this.lives = 3;
+        game.physics.enable(this, Phaser.Physics.ARCADE);
+        this.body.maxVelocity.setTo(PLAYER.MAX_SPEED, PLAYER.MAX_SPEED);
+        game.add.existing(this);
     }
 
-    if (this.x < 50) {
-      this.x = 50;
+    update() {
+        if (this.x > this.game.width - 50) {
+            this.x = this.game.width - 50;
+        }
+
+        if (this.x < 50) {
+            this.x = 50;
+        }
     }
-  }
 
-  moveLeft() {
-    this.body.velocity.x = -PLAYER.MAX_SPEED;
-  }
+    moveLeft() {
+        this.body.velocity.x = -PLAYER.MAX_SPEED;
+    }
 
-  moveRight() {
-    this.body.velocity.x = PLAYER.MAX_SPEED;
-  }
+    moveRight() {
+        this.body.velocity.x = PLAYER.MAX_SPEED;
+    }
 }
--- scripts/game-models/weapon.js
+++ scripts/game-models/weapon.js
@@ -12,71 +12,24 @@ class Weapon extends Phaser.Group {
 
     fireBullet(weaponLevel, player, factorDifficulty) {
         if (this.game.time.now < this.timer) {
-            return false;
+            return;
         }
 
-        switch (weaponLevel) {
-            case 1:
-                {
-                    let bullet = this.getFirstExists(false);
+        let bulletsCount = weaponLevel + weaponLevel - 1,
+            fireSpeed = bulletsCount * 100,
+            angles = [0, 20, -20, 40, -40],
+            bullet;
 
-                    if (bullet) {
-                        bullet.reset(player.x, player.y);
-                        bullet.body.velocity.y = -BULLET.SPEED * factorDifficulty;
+        for (let i = 0; i < bulletsCount; i += 1) {
+            bullet = this.getFirstExists(false);
+            if (bullet) {
+                bullet.reset(player.x, player.y);
+                bullet.angle = angles[i];
 
-                        this.timer = this.game.time.now + (BULLET.SPACING / factorDifficulty);
-                    }
-                }
-                break;
-
-            case 2:
-                {
-                    for (let i = 0; i < 3; i += 1) {
-                        let bullet = this.getFirstExists(false);
-                        if (bullet) {
-                            //  Make bullet come out of tip of ship with right angle
-                            let bulletOffset = 20 * Math.sin(this.game.math.degToRad(player.angle));
-                            bullet.reset(player.x + bulletOffset, player.y);
-                            //  "Spread" angle of 1st and 3rd bullets
-                            let spreadAngle;
-                            if (i === 0) spreadAngle = -20;
-                            if (i === 1) spreadAngle = 0;
-                            if (i === 2) spreadAngle = 20;
-                            bullet.angle = player.angle + spreadAngle;
-                            this.game.physics.arcade.velocityFromAngle(spreadAngle - 90, BULLET.SPEED, bullet.body.velocity);
-                            bullet.body.velocity.y = -BULLET.SPEED * factorDifficulty;
-                        }
-                        this.timer = game.time.now + ((BULLET.SPACING + 300) / factorDifficulty);
-                    }
-                }
-                break;
-
-            case 3:
-                {
-                    for (let i = 0; i < 5; i += 1) {
-                        let bullet = this.getFirstExists(false);
-                        if (bullet) {
-                            //  Make bullet come out of tip of ship with right angle
-                            let bulletOffset = 20 * Math.sin(this.game.math.degToRad(player.angle));
-                            bullet.reset(player.x + bulletOffset, player.y);
-                            //  "Spread" angle of 1st and 3rd bullets
-                            var spreadAngle;
-                            if (i === 0) spreadAngle = -20;
-                            if (i === 1) spreadAngle = 0;
-                            if (i === 2) spreadAngle = 20;
-                            if (i === 3) spreadAngle = 40;
-                            if (i === 4) spreadAngle = -40;
-                            bullet.angle = player.angle + spreadAngle;
-                            this.game.physics.arcade.velocityFromAngle(spreadAngle - 90, BULLET.SPEED, bullet.body.velocity);
-                            bullet.body.velocity.y = -BULLET.SPEED * factorDifficulty;
-                        }
-                        this.timer = game.time.now + ((BULLET.SPACING + 300) / factorDifficulty);
-                    }
-                }
-                break;
-
-            default:
-                throw new Error("Weapon level not defined.");
+                this.game.physics.arcade.velocityFromAngle(bullet.angle - 90, BULLET.SPEED, bullet.body.velocity);
+                bullet.body.velocity.y = -BULLET.SPEED * factorDifficulty;
+            }
+            this.timer = game.time.now + ((BULLET.SPACING + fireSpeed) / factorDifficulty);
         }
     }
 }
